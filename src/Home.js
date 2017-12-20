81f70d3afe79156f4868d97dee6bdec58c058676
gmakersteam <gmakers.team@gmail.com>
2017-12-20 14:34:13 -0500

    api search

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 // import Utils from './Utils.js';
+import {searchISBN} from './actions';
 
-import {BrowserRouter, Route, Switch, NavLink, Redirect} from 'react-router-dom'
+// import {BrowserRouter, Route, Switch, NavLink, Redirect} from 'react-router-dom'
 
 const Home = () => {
+     const onSubmit = (e) => {
+		e.preventDefault();
+		console.log ( 'this..', this);//con truco, es el connect el this.
+    searchISBN(this.isbn.value);
+     this.isbn.value = "";
+  }
     return (
         <div>
-            <h1>Hola Soy el Home</h1>
+             <form onSubmit = {onSubmit}>
+        <div className="form-group">
+          <label >Name User: </label>
+          <input type="text" className="form-control" ref={(e) => this.isbn = e} />
+        </div>
+        <button type="submit" className="btn btn-default">Submit</button>
+      </form>
         </div>
     )
 }
