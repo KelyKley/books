abad38bf17f9ac25849cdd9f110718239a2ef6a4
KelyKley <kelyanamurohuamani@gmail.com>
2017-12-20 14:35:32 -0500

    primera vista

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,52 @@ import React, {Component} from 'react';
 
 import {BrowserRouter, Route, Switch, NavLink, Redirect} from 'react-router-dom'
 
-const Home = () => {
+const Navbar = () => {
     return (
-        <div>
-            <h1>Hola Soy el Home</h1>
-        </div>
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-book"></span></a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+                </div>
+            </div>
+            </nav>
     )
 }
 
-export default Home;
+const Search = () => {
+    return (
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+            <input id="email" type="text" class="search form-control" name="email" placeholder="Email"/>
+        </div>
+    );
+}
+
+const Results = () => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+const Book = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Search/>
+        </div>
+    );
+}
+export default Book;
